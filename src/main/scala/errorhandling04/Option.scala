16d0f2480276da80fa3f71a08c46ec9a25ae3e19
honey.and.sw <honey.and.sw@gmail.com>
2017-02-28 23:57:24 +0900

    4.3 Option - variance

--- src/main/scala/errorhandling04/Option.scala
+++ src/main/scala/errorhandling04/Option.scala
@@ -1,6 +1,9 @@
 package fpis.errorhandling04
 
 sealed trait Option[+A] {
+  /***************
+    * 연습문제 4.1 *
+    **************/
   def map[B](f: A => B): Option[B] = this match {
     case Some(get) => Some(f(get))
     case _ => None
@@ -21,3 +24,19 @@ sealed trait Option[+A] {
 case class Some[+A](get: A) extends Option[A]
 
 case object None extends Option[Nothing]
+
+object Option {
+
+  /**
+    * 연습문제 4.2
+    */
+  def variance(xs: Seq[Double]): Option[Double] =
+    Some(xs)
+      .filter(_.length > 0)
+      .flatMap { seq =>
+        val m = seq.foldLeft(0.0)(_ + _) / seq.length
+        val m2 = seq.map(d => math.pow(d - m, 2)).foldLeft(0.0)(_ + _) / seq.length
+        Some(m2)
+      }
+
+}
