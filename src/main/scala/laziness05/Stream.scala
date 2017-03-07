3f95b7fb5b1e8dfa874bfff7c3755aa25a8721d3
honey.and.sw <honey.and.sw@gmail.com>
2017-03-08 00:27:27 +0900

    5 Stream - 무한 스트림

--- src/main/scala/laziness05/Stream.scala
+++ src/main/scala/laziness05/Stream.scala
@@ -6,6 +6,11 @@ sealed trait Stream[+A] {
     case Cons(h, t) => Some(h())
   }
 
+  /**
+    * 연습문제 5.6
+    */
+  def headOption2: Option[A] = foldRight[Option[A]](None)((a, acc) => Some(a))
+
   def exists(p: A => Boolean): Boolean = foldRight(false)((a, acc) => p(a) || acc)
 //  this match {
 //    case Cons(h, t) => p(h()) || t().exists(p)
@@ -39,7 +44,18 @@ sealed trait Stream[+A] {
     * 연습문제 5.2
     * 처음 n개의 요소를 건너뛴 Stream을 돌려주는 함수
     */
-  def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if (n > 0) => t().drop(n - 1)
+    case _ => this
+  }
+
+  /**
+    * 연습문제 5.3
+    * 연습문제 5.5
+    * 주어진 술어를 만족하는 선행 요소들을 모두 돌려주는 함수
+    */
+  def takeWhile(p: A => Boolean): Stream[A] =
+    foldRight[Stream[A]](Stream.empty)((a, acc) => if (p(a)) Stream.apply(a) else Stream.cons(a, acc))
 
   /**
     * 연습문제 5.4
@@ -60,4 +76,33 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  /**
+    * 연습문제 5.8
+    */
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  /**
+    * 연습문제 5.9
+    */
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  /**
+    * 연습문제 5.10
+    */
+  def fibs(): Stream[Int] = {
+    def fibs2(i1: Int, i2: Int): Stream[Int] = cons(i1, fibs2(i2, i1 + i2))
+    fibs2(0, 1)
+  }
+
+  /**
+    * 연습문제 5.11
+    * @param z 초기상태
+    * @param f 다음 상태 및 (생성된 스트림 안의) 다음 값을 산출하는 함수
+    */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z).flatMap{ as =>
+      Some(cons(as._1, unfold(as._2)(f)))
+    }.getOrElse(empty)
+  }
 }
--- src/test/scala/laziness05/StreamSpecs.scala
+++ src/test/scala/laziness05/StreamSpecs.scala
@@ -8,12 +8,22 @@ import scala.collection.immutable.List
 class StreamSpecs extends FunSuite {
 
   def fixture = new {
+    def pow(i: Int, p: Int): Double = {
+      // println(s"pow ${i}, ${p}")
+      Math.pow(i, p)
+    }
+
     val i = {
       println("print i...") // println is done only once.
       2
     }
-    val stream = Stream.apply(
-      Math.pow(i, 2), Math.pow(i, 3), Math.pow(i, 4), Math.pow(i, 5)
+
+    // Stream of Math.pow(i, 2), Math.pow(i, 3), Math.pow(i, 4), Math.pow(i, 5)
+    val stream = Stream.cons(
+      pow(i, 2), Stream.cons(
+        pow(i, 3), Stream.cons(
+          pow(i, 4), Stream.cons(
+            pow(i, 5), Stream.empty)))
     )
   }
 
@@ -29,10 +39,58 @@ class StreamSpecs extends FunSuite {
     f.stream.take(5).toList should equal (List(4.0, 8.0, 16.0, 32.0))
   }
 
+  test("drop") {
+    val f = fixture
+    f.stream.drop(2).toList should equal (List(16.0, 32.0))
+    f.stream.drop(0).toList should equal (List(4.0, 8.0, 16.0, 32.0))
+    f.stream.drop(5).toList should equal (Nil)
+  }
+
+  test("takeWhile") {
+    val f = fixture
+    f.stream.takeWhile(_ >= 4).toList should equal (List(4.0))
+    f.stream.takeWhile(_ > 10).toList should equal (List(4.0, 8.0, 16.0))
+  }
+
   test("forAll") {
     val f = fixture
     f.stream.forAll(d => d < 10.0) should equal (false)
     f.stream.forAll(d => d < 33.0) should equal (true)
   }
 
+  test("headOption") {
+    val f = fixture
+    f.stream.headOption should equal (Some(4.0))
+    Stream.empty.headOption should equal (None)
+  }
+
+  test("headOption2") {
+    val f = fixture
+    f.stream.headOption2 should equal (Some(4.0))
+    Stream.empty.headOption2 should equal (None)
+  }
+
+  test("constant") {
+    val constant = Stream.constant(1)
+    constant.take(3).toList should equal (List(1, 1, 1))
+  }
+
+  test("from") {
+    val from = Stream.from(1)
+    from.take(5).toList should equal (List(1, 2, 3, 4, 5))
+  }
+
+  test("fibs") {
+    Stream.fibs().take(5).toList should equal (List(0, 1, 1, 2, 3))
+  }
+
+  test("unfold") {
+    val unfold1 = Stream.unfold(1)(i => Some((i + 1/*다음 값*/, i + 10/*다음 상태*/)))
+    unfold1.take(5).toList should equal (List(2, 12, 22, 32, 42))
+
+    val unfold2 = Stream.unfold(1) { i =>
+      if (i > 20) None else Some((i + 1, i + 10))
+    }
+    unfold2.take(5).toList should equal (List(2, 12))
+  }
 }
