438c4d17e918643835f5b99bf4a30bb6ba5cbc17
honey.and.sw <honey.and.sw@gmail.com>
2017-03-28 21:14:39 +0900

    7 Parallelism - Par의 조합기

--- src/main/scala/parallelism07/Par.scala
+++ src/main/scala/parallelism07/Par.scala
@@ -1,18 +1,43 @@
 package parallelism07
 
-import java.util.concurrent.{Future, ExecutorService}
+import java.util.concurrent.{Callable, TimeUnit, Future, ExecutorService}
 
 object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = ???
+  /**
+    * Future를 아주 간단하게 구현한 상수 get의 Wrapper
+    */
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isCancelled: Boolean = false
+    override def get(timeout: Long, unit: TimeUnit): A = get
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+    override def isDone: Boolean = true
+  }
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
-  def fork[A](a: => Par[A]): Par[A] = ???
+  def map[A, B](a: Par[A])(f: A => B): Par[B] = map2(a, unit(()))((a, _) => f(a))
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+    val af: Future[A] = a(es)
+    val bf: Future[B] = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
+  def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => {
+    es.submit(new Callable[A] {
+      override def call(): A = a(es).get
+    })
+  }
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+  def run[A](es: ExecutorService)(a: Par[A]): Future[A] = a(es)
+
+  /**
+    * 연습문제 7.4
+    */
+  def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => lazyUnit(f(a))
 }
